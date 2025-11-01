5f2af4936ca420fd2235e57baee42bb6a37fe20f
shoaib-67 <shoaibs17677@gmail.com>
2025-11-01 20:29:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// login 
+document.getElementById("loginButton").addEventListener("click",function (e){
+    e.preventDefault()
+    const mobileNumber = 12345678910
+    const pin = 1234
+    
+    const mobileNumberValue = document.getElementById
+    ("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+
+    const pinNumberValue = document.getElementById("pin-number").value
+
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted===pin){
+        window.location.href="./home.html"
+    }
+
+    else{
+        alert("Invalid Credentials!")
+    }
+})
